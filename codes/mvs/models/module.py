4117336e5be883e008b144e679f6b9cc531a0b51
miaoji <jiaweimiao221@gmail.com>
2021-12-03 18:46:37 +0100

    final

--- codes/mvs/models/module.py
+++ codes/mvs/models/module.py
@@ -9,31 +9,31 @@ class FeatureNet(nn.Module):
         super(FeatureNet, self).__init__()
         # TODO
         self.layers = nn.Sequential(
-            nn.Conv2d(3, 8, (3,3), stride=1),
+            nn.Conv2d(3, 8, (3,3), stride=1, padding=1),
             nn.BatchNorm2d(8),
             nn.ReLU(),
-            nn.Conv2d(8, 8, (3,3), stride=1),
+            nn.Conv2d(8, 8, (3,3), stride=1, padding=1),
             nn.BatchNorm2d(8),
             nn.ReLU(),
-            nn.Conv2d(8, 16, (5, 5), stride=2),
+            nn.Conv2d(8, 16, (5, 5), stride=2, padding=2),
             nn.BatchNorm2d(16),
             nn.ReLU(),
-            nn.Conv2d(16, 16, (3, 3), stride=1),
+            nn.Conv2d(16, 16, (3, 3), stride=1, padding=1),
             nn.BatchNorm2d(16),
             nn.ReLU(),
-            nn.Conv2d(16, 16, (3, 3), stride=1),
+            nn.Conv2d(16, 16, (3, 3), stride=1, padding=1),
             nn.BatchNorm2d(16),
             nn.ReLU(),
-            nn.Conv2d(16, 32, (5, 5), stride=2),
+            nn.Conv2d(16, 32, (5, 5), stride=2, padding=2),
             nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.Conv2d(32, 32, (3, 3), stride=1),
+            nn.Conv2d(32, 32, (3, 3), stride=1, padding=1),
             nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.Conv2d(32, 32, (3, 3), stride=1),
+            nn.Conv2d(32, 32, (3, 3), stride=1, padding=1),
             nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.Conv2d(32, 32, (3, 3), stride=1)
+            nn.Conv2d(32, 32, (3, 3), stride=1, padding=1)
 
         )
 
@@ -49,43 +49,29 @@ class SimlarityRegNet(nn.Module):
     def __init__(self, G):
         super(SimlarityRegNet, self).__init__()
         # TODO
-        self.layers0 = nn.Sequential(
-            nn.Conv2d(G, 8, (3,3), stride=1),
-            nn.ReLU()
-        )
-        self.layers1 = nn.Sequential(
-            nn.Conv2d(8, 16, (3, 3), stride=2),
-            nn.ReLU()
-        )
-        self.layers2 = nn.Sequential(
-            nn.Conv2d(16, 32, (3, 3), stride=2),
-            nn.ReLU()
-        )
-        self.layers3 = nn.Sequential(
-            nn.ConvTranspose2d(32, 16, (3, 3), stride=2),
-        )
-        self.layers4 = nn.Sequential(
-            nn.ConvTranspose2d(16, 8, (3, 3), stride=2),
-        )
-        self.layersfinal = nn.Sequential(
-            nn.Conv2d(8, 1, (3, 3), stride=1),
-        )
-
+        self.layers0 = nn.Conv2d(G, 8, (3,3), stride=1, padding=1)
+        self.layers1 = nn.Conv2d(8, 16, (3, 3), stride=2, padding=1)
+        self.layers2 = nn.Conv2d(16, 32, (3, 3), stride=2, padding=1)
+        self.layers3 = nn.ConvTranspose2d(32, 16, (3, 3), stride=2, padding=1, output_padding=1)
+        self.layers4 = nn.ConvTranspose2d(16, 8, (3, 3), stride=2, padding=1, output_padding=1)
+        self.layersfinal = nn.Conv2d(8, 1, (3, 3), stride=1, padding=1)
+        self.relu=nn.ReLU(True)
 
     def forward(self, x):
         # x: [B,G,D,H,W]
         # out: [B,D,H,W]
         # TODO
         B, G, D, H, W = x.size()
-        x = x.view(B, G, D, H, W)
-        x = x.shape(B, D, H, W)
-        C0 = self.layers0(x)
-        C1 = self.layers1(C0)
-        C2 = self.layers2(C1)
+
+        x = x.reshape(B,G,D*H,W)
+        C0 = self.relu(self.layers0(x))
+        C1 = self.relu(self.layers1(C0))
+        C2 = self.relu(self.layers2(C1))
         C3 = self.layers3(C2)
-        C4 = self.layers4(C3+C1)
-        s_bar = self.layersfinal(C4+C0)
-        return s_bar
+        C4 = self.layers4(C3 + C1)
+        s_bar = self.layersfinal(C4 + C0)
+        outS = s_bar.reshape(B,D,H,W)
+        return outS
 
 def warping(src_fea, src_proj, ref_proj, depth_values):
     # src_fea: [B, C, H, W]
@@ -110,7 +96,9 @@ def warping(src_fea, src_proj, ref_proj, depth_values):
         xyz = torch.unsqueeze(xyz, 0).repeat(B, 1, 1)
 
         rot_xyz = torch.matmul(rot, xyz)
-        rot_depth_xyz = rot_xyz.unsqueeze(2).repeat(1, 1, D, 1) * depth_values.size(1)  # [B, 3, Ndepth, H*W]
+        rot_depth_xyz = rot_xyz.unsqueeze(2).repeat(1, 1, D, 1)
+        depth = depth_values.unsqueeze(1).repeat(1,3,1).unsqueeze(3).repeat(1,1,1,H*W)
+        rot_depth_xyz = rot_depth_xyz*depth
         proj_xyz = rot_depth_xyz + trans.view(B, 3, 1, 1)
         # avoid negative depth
         negative_depth_mask = proj_xyz[:, 2:] <= 1e-3
@@ -123,7 +111,7 @@ def warping(src_fea, src_proj, ref_proj, depth_values):
         proj_xy = torch.stack((proj_x_normalized, proj_y_normalized), dim=3)
         grid = proj_xy
 
-    warped_src_fea = F.grid_sample(
+        warped_src_fea = F.grid_sample(
         src_fea,
         grid.view(B, D * H, W, 2),
         mode="bilinear",
@@ -139,16 +127,11 @@ def group_wise_correlation(ref_fea, warped_src_fea, G):
     # warped_src_fea: [B,C,D,H,W]
     # out: [B,G,D,H,W]
     # TODO
-    B, C, H, W = ref_fea.size()
-    D = warped_src_fea.size(2)
+    B, C, D, H, W = warped_src_fea.size()
+    assert (C % G == 0)
     ref_fea_g = ref_fea.view(B, G, C // G, 1, H, W)
     warped_src_fea_g = warped_src_fea.view(B, G, C // G, D, H, W)
-
     similarity = (warped_src_fea_g * ref_fea_g).mean(2)
-
-#    ref_fea_s = np.split(ref_fea,[1,G,1,1])
-#    warped_src_fea_s = np.split(warped_src_fea,[1,G,1,1,1])
-#    S_g = G*np.dot(ref_fea_s, warped_src_fea_s)/C
 
     return similarity
 
@@ -158,8 +141,7 @@ def depth_regression(p, depth_values):
     # p: probability volume [B, D, H, W]
     # depth_values: discrete depth values [B, D]
     # TODO
-
-    return torch.sum(p*depth_values.view(depth_values.shape[0], 1, 1), dim=1).unsqueeze(1)
+    return torch.sum(p * depth_values.view(depth_values.shape[0],depth_values.shape[1],1,1), dim=1)
 
 def mvs_loss(depth_est, depth_gt, mask):
     # depth_est: [B,1,H,W]
@@ -167,11 +149,9 @@ def mvs_loss(depth_est, depth_gt, mask):
     # mask: [B,1,H,W]
     # TODO
     loss = 0
-    for i in range(0, 4):
-        mask_i = mask[f"stage_{i}"] > 0.5
-        gt_depth = depth_gt[f"stage_{i}"][mask_i]
-        for depth in depth_est[i]:
-            loss = loss + F.smooth_l1_loss(depth[mask_i], gt_depth, reduction="mean")
+    gt_masked = depth_gt[mask>0.5]
+    est_masked = depth_est[mask>0.5]
+    loss = F.smooth_l1_loss(est_masked, gt_masked)
 
     return loss
 
